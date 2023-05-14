604ca0d1428a434a5a523a8ed41a374344473086
BoGyum Kim | 김보겸 <109015852+seedspirit@users.noreply.github.com>
2023-05-14 12:43:03 +0900

    Refactor: Make Crawling Process more readable & Functionalize AWSConfigure Process

--- main.go
+++ main.go
@@ -44,6 +44,122 @@ func makingFinalFileName(lineNum string) string {
 	return finalFileName
 }
 
+// 에러 체킹용 함수
+func checkErr(err error) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+// AWS S3 사용을 위한 credential 설정 & client 생성
+func AWSConfigure() BucketBasics {
+	staticProvider := credentials.NewStaticCredentialsProvider(
+		"AKIASUBPVZPZ5UEXUOFU",
+		"i5bV0MmEX2PEGr+flLlY/7VXdozkfL9OEZsrT9ii",
+		"")
+
+	sdkConfig, err := config.LoadDefaultConfig(
+		context.Background(),
+		config.WithCredentialsProvider(staticProvider),
+	)
+	checkErr(err)
+
+	s3Client := s3.NewFromConfig(sdkConfig)
+	bucketBasics := BucketBasics{s3Client}
+
+	return bucketBasics
+}
+
+// struct를 json 형태로 변환 후 makingFileName에서 나온 이름으로 S3에 파일 업로드
+func S3Uploader(data []map[string]string, basics BucketBasics, finalFileName string) error {
+	// data가 struct 형태일때는 이상하게 marshal이 되더니, map으로 바꾸니까 한방에 marshal이 잘 됨. 이유가 뭘까?
+	content, err := json.Marshal(data)
+	if err != nil {
+		log.Fatalln("JSON marshaling failed: %s", err)
+	}
+
+	// json 바이트 스트림을 S3에 업로드
+	_, err = basics.S3Client.PutObject(context.TODO(), &s3.PutObjectInput{
+		Bucket: aws.String("bucketestmy"),
+		Key:    aws.String("bmt/" + finalFileName),
+		Body:   bytes.NewReader(content),
+	})
+	if err != nil {
+		return fmt.Errorf("failed to upload, %v", err)
+	}
+	fmt.Println(finalFileName + "file successfully uploaded in S3")
+	return nil
+}
+
+// S3에서 파일 다운로드 후 json 데이터를 파싱하여 golang 자료 구조에 맞게 변환
+func S3Downloader(basics BucketBasics) (map[string][]map[string]interface{}, error) {
+	result, err := basics.S3Client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String("bucketestmy"),
+		Key:    aws.String("bmt/subway_information.json"),
+	})
+	if err != nil {
+		log.Printf("Couldn't get object. Here's why: %v", err)
+		return nil, err
+	}
+
+	defer result.Body.Close()
+
+	body, err := io.ReadAll(result.Body)
+	if err != nil {
+		log.Printf("failed to read response body: %v", err)
+		return nil, err
+	}
+
+	var INFO = map[string][]map[string]interface{}{}
+	err = json.Unmarshal(body, &INFO)
+	checkErr(err)
+	return INFO, nil
+}
+
+// chromedp 설정 & 홈페이지 접속 후 시간표 탭 클릭하여 타겟 페이지 접속
+func getHTMLContents(URL string, lineNum string, stationNm string) string {
+	// settings for crawling
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.NoSandbox,
+		chromedp.Flag("disable-setuid-sandbox", true),
+		chromedp.Flag("disable-dev-shm-usage", true),
+		chromedp.Flag("single-process", true),
+		chromedp.Flag("no-zygote", true),
+	)
+
+	alloCtx, _ := chromedp.NewExecAllocator(context.Background(), opts...)
+
+	ctx, cancel := chromedp.NewContext(alloCtx, chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	var htmlContent string
+
+	ch := chromedp.WaitNewTarget(ctx, func(i *target.Info) bool {
+		return strings.Contains(i.URL, "/timetable/web/")
+	})
+
+	// 크롤링 대상 페이지에 접속하기 위해 URL 접속 -> 클릭
+	err := chromedp.Run(ctx,
+		chromedp.Navigate(URL),
+		// 클릭해야 할 부분이 나올때까지 기다리기
+		chromedp.WaitVisible(".end_footer_area"),
+		chromedp.Click("body > div.app > div > div > div > div.end_section.station_info_section > div.at_end.sofzqce > div > div.c10jv2ep.wrap_btn_schedule.schedule_time > button"),
+	)
+	checkErr(err)
+
+	// 클릭으로 새로운 탭이 생긴 곳으로 컨텍스트 옮기기 -> OuterHTML 추출
+	newContext, cancel := chromedp.NewContext(ctx, chromedp.WithTargetID(<-ch))
+	defer cancel()
+	if err := chromedp.Run(newContext,
+		chromedp.WaitReady(".table_schedule", chromedp.ByQuery),
+		chromedp.OuterHTML(".schedule_wrap", &htmlContent, chromedp.ByQuery),
+	); err != nil {
+		panic(err)
+	}
+
+	return htmlContent
+}
+
 // 타깃 페이지 HTML에서 필요한 정보만 추출 후 정리하기
 func crawler(htmlContent string, lineNum string, stationNm string) {
 	doc, _ := goquery.NewDocumentFromReader(strings.NewReader(htmlContent))
@@ -100,107 +216,13 @@ func crawler(htmlContent string, lineNum string, stationNm string) {
 
 }
 
-// 에러 체킹용 함수
-func checkErr(err error) {
-	if err != nil {
-		log.Fatalln(err)
-	}
-}
-
+// subway_information에서 각 호선의 네이버코드와 역이름 정보 가져오기 -> 해당 역이름 페이지로 접속하여 HTML 획득 -> crawler로 스크래핑
 func runCrawler(val map[string]interface{}, baseURL string, lineNum string) {
 	// val안의 값들은 interface이기 때문에 type assertion 필요
 	naverCode := int(val["naverCode"].(float64)) // 왜인지 모르겠지만 처음 파일에서 interface로 값 가져올 때 float64로 가져와짐
 	stationNm := val["stationNm"].(string)
 	URL := baseURL + strconv.Itoa(naverCode) + "/home"
-	getPage(URL, lineNum, stationNm)
-}
-
-// struct를 json 형태로 변환 후 makingFileName에서 나온 이름으로 S3에 파일 업로드
-func S3Uploader(data []map[string]string, basics BucketBasics, finalFileName string) error {
-	// data가 struct 형태일때는 이상하게 marshal이 되더니, map으로 바꾸니까 한방에 marshal이 잘 됨. 이유가 뭘까?
-	content, err := json.Marshal(data)
-	if err != nil {
-		log.Fatalln("JSON marshaling failed: %s", err)
-	}
-
-	// json 바이트 스트림을 S3에 업로드
-	_, err = basics.S3Client.PutObject(context.TODO(), &s3.PutObjectInput{
-		Bucket: aws.String("bucketestmy"),
-		Key:    aws.String("bmt/" + finalFileName),
-		Body:   bytes.NewReader(content),
-	})
-	if err != nil {
-		return fmt.Errorf("failed to upload, %v", err)
-	}
-	fmt.Println(finalFileName + "file successfully uploaded in S3")
-	return nil
-}
-
-// S3에서 파일 다운로드 후 json 데이터를 파싱하여 golang 자료 구조에 맞게 변환
-func S3Downloader(basics BucketBasics) (map[string][]map[string]interface{}, error) {
-	result, err := basics.S3Client.GetObject(context.TODO(), &s3.GetObjectInput{
-		Bucket: aws.String("bucketestmy"),
-		Key:    aws.String("bmt/subway_information.json"),
-	})
-	if err != nil {
-		log.Printf("Couldn't get object. Here's why: %v", err)
-		return nil, err
-	}
-
-	defer result.Body.Close()
-
-	body, err := io.ReadAll(result.Body)
-	if err != nil {
-		log.Printf("failed to read response body: %v", err)
-		return nil, err
-	}
-
-	var INFO = map[string][]map[string]interface{}{}
-	err = json.Unmarshal(body, &INFO)
-	checkErr(err)
-	return INFO, nil
-}
-
-// chromedp context 생성 & waitnewtarget 설정 -> 크롤링해서 정보 저장
-func getPage(URL string, lineNum string, stationNm string) {
-	// settings for crawling
-	opts := append(chromedp.DefaultExecAllocatorOptions[:],
-		chromedp.NoSandbox,
-		chromedp.Flag("disable-setuid-sandbox", true),
-		chromedp.Flag("disable-dev-shm-usage", true),
-		chromedp.Flag("single-process", true),
-		chromedp.Flag("no-zygote", true),
-	)
-
-	alloCtx, _ := chromedp.NewExecAllocator(context.Background(), opts...)
-
-	ctx, cancel := chromedp.NewContext(alloCtx, chromedp.WithLogf(log.Printf))
-	defer cancel()
-
-	var htmlContent string
-
-	ch := chromedp.WaitNewTarget(ctx, func(i *target.Info) bool {
-		return strings.Contains(i.URL, "/timetable/web/")
-	})
-
-	// 크롤링 대상 페이지에 접속하기 위해 URL 접속 -> 클릭
-	err := chromedp.Run(ctx,
-		chromedp.Navigate(URL),
-		// 클릭해야 할 부분이 나올때까지 기다리기
-		chromedp.WaitVisible(".end_footer_area"),
-		chromedp.Click("body > div.app > div > div > div > div.end_section.station_info_section > div.at_end.sofzqce > div > div.c10jv2ep.wrap_btn_schedule.schedule_time > button"),
-	)
-	checkErr(err)
-
-	// 클릭으로 새로운 탭이 생긴 곳으로 컨텍스트 옮기기 -> OuterHTML 추출
-	newContext, cancel := chromedp.NewContext(ctx, chromedp.WithTargetID(<-ch))
-	defer cancel()
-	if err := chromedp.Run(newContext,
-		chromedp.WaitReady(".table_schedule", chromedp.ByQuery),
-		chromedp.OuterHTML(".schedule_wrap", &htmlContent, chromedp.ByQuery),
-	); err != nil {
-		panic(err)
-	}
+	htmlContent := getHTMLContents(URL, lineNum, stationNm)
 
 	// 페이지 소스 크롤링 & 필요한 정보 정리하기
 	crawler(htmlContent, lineNum, stationNm)
@@ -209,20 +231,9 @@ func getPage(URL string, lineNum string, stationNm string) {
 func HandleRequest(_ context.Context) (string, error) {
 	start := time.Now()
 
-	staticProvider := credentials.NewStaticCredentialsProvider(
-		"AWS_ACCESS_KEY",
-		"AWS_SECRET_KEY",
-		"")
+	bucktBasics := AWSConfigure()
 
-	sdkConfig, err := config.LoadDefaultConfig(
-		context.Background(),
-		config.WithCredentialsProvider(staticProvider),
-	)
-	checkErr(err)
-
-	s3Client := s3.NewFromConfig(sdkConfig)
-	basics := BucketBasics{s3Client}
-	INFO, err := S3Downloader(basics)
+	INFO, err := S3Downloader(bucktBasics)
 	checkErr(err)
 
 	// INFO에서 key(호선 명) 뽑아내기
@@ -231,6 +242,8 @@ func HandleRequest(_ context.Context) (string, error) {
 		targetLines = append(targetLines, k)
 	}
 	sort.Strings(targetLines)
+	// Lambda timeout으로 인해 일부만 크롤링: 1호선~5호선 / 6호선~신림선 (6분)
+	targetLines = targetLines[:5]
 
 	// 각 역의 정보를 바탕으로 크롤링 시작. for문을 돌며 호선 이름과 그 호선에 해당하는 역 정보 가져오고 -> 그거 바탕으로 크롤링
 	var baseURL string = "https://pts.map.naver.com/end-subway/ends/web/"
@@ -240,8 +253,8 @@ func HandleRequest(_ context.Context) (string, error) {
 		data = make([]map[string]string, 0, len(info)) // 각 호선의 데이터만 담을 수 있도록 슬라이스 초기화
 		fmt.Println("타겟 라인: ", lineNum, " 크롤링 시작")
 
-		// semaphore로 go 루틴 개수 20개로 제한, 각 go 루틴 실행 종료 시점 수집을 위한 채널 생성
-		sem := make(chan struct{}, 10)
+		// semaphore로 go 루틴 개수 15개로 제한, 각 go 루틴 실행 종료 시점 수집을 위한 채널 생성
+		sem := make(chan struct{}, 15)
 		done := make(chan struct{}, len(info))
 
 		for i, val := range info {
@@ -253,8 +266,8 @@ func HandleRequest(_ context.Context) (string, error) {
 				done <- struct{}{} // go 루틴이 종료되었음을 알리기 위해 done 채널에 값 전송
 			}(val)
 
-			if (i+1)%10 == 0 { // 네이버 서버에 부담을 주지 않도록 고루틴 20개마다 sleep
-				for j := 0; j < 10; j++ {
+			if (i+1)%15 == 0 { // 네이버 서버에 부담을 주지 않도록 고루틴 15개마다 sleep
+				for j := 0; j < 15; j++ {
 					<-done
 				}
 				fmt.Println("--- 3초 휴식 ---")
@@ -263,7 +276,7 @@ func HandleRequest(_ context.Context) (string, error) {
 		}
 
 		// 모든 go 루틴이 종료될 때까지 done 채널에서 값 수신
-		for i := 0; i < len(info)%10; i++ {
+		for i := 0; i < len(info)%15; i++ {
 			<-done
 		}
 
@@ -271,7 +284,7 @@ func HandleRequest(_ context.Context) (string, error) {
 		finalFilename := makingFinalFileName(lineNum)
 
 		// 파일을 S3에 업로드
-		err := S3Uploader(data, basics, finalFilename)
+		err := S3Uploader(data, bucktBasics, finalFilename)
 		if err != nil {
 			fmt.Printf("S3Uploader failed, %v", err)
 		}
